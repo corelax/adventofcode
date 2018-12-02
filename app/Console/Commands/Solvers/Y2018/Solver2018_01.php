7cf10f29699b162733d4483deb67bbf1b3d3b71d
corelax <5426101+corelax@users.noreply.github.com>
2018-12-02 21:30:17 +0900

    feat: add part2 solver

--- app/Console/Commands/Solvers/Y2018/Solver2018_01.php
+++ app/Console/Commands/Solvers/Y2018/Solver2018_01.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands\Solvers\Y2018;
 use Illuminate\Console\Command;
 use Storage;
 use SplFileObject;
+use SeekableIterator;
 
 class Solver2018_01 extends Command
 {
@@ -41,14 +42,32 @@ class Solver2018_01 extends Command
     {
         //
         $input = new SplFileObject(storage_path('app/input/2018/day01/input.dat'));
-        $input->setFlags(SplFileObject::DROP_NEW_LINE);
-        echo $this->calc($input) . PHP_EOL;
+        $input->setFlags(SplFileObject::DROP_NEW_LINE | SplFileObject::READ_AHEAD | SplFileObject::SKIP_EMPTY);
+        echo "part1 : " . $this->calc($input) . PHP_EOL;
+        echo "part2 : " . $this->calc2($input) . PHP_EOL;
+        // echo $this->calc2((new \ArrayObject([+3, +3, +4, -2, -4]))->getIterator()) . PHP_EOL;
     }
 
     private function calc(iterable $input) {
         $result = 0;
         foreach ($input as $data) {
             $result += intval($data);
+        }
+        return $result;
+    }
+
+    private function calc2(SeekableIterator $input) {
+        $history = [0 => true];
+        $result = 0;
+        while(true) {
+            foreach ($input as $data) {
+                $result += intval($data);
+                if (isset($history[$result])) {
+                    return $result;
+                }
+                $history[$result] = true;
+            }
+            $input->rewind();
         }
         return $result;
     }
