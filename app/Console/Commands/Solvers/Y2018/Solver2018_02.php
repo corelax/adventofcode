e42d955ca9099f1d3ac4908a90d2819d85fafa2c
corelax <5426101+corelax@users.noreply.github.com>
2018-12-03 01:05:29 +0900

    feat: add solver 2018 day02 part2

--- app/Console/Commands/Solvers/Y2018/Solver2018_02.php
+++ app/Console/Commands/Solvers/Y2018/Solver2018_02.php
@@ -5,7 +5,6 @@ namespace App\Console\Commands\Solvers\Y2018;
 use Illuminate\Console\Command;
 use Storage;
 use SplFileObject;
-use SeekableIterator;
 
 class Solver2018_02 extends Command
 {
@@ -44,6 +43,7 @@ class Solver2018_02 extends Command
         $input = new SplFileObject(storage_path('app/input/2018/day02/input.dat'));
         $input->setFlags(SplFileObject::DROP_NEW_LINE | SplFileObject::READ_AHEAD | SplFileObject::SKIP_EMPTY);
         echo "part1 : " . $this->calc($input) . PHP_EOL;
+        echo "part2 : " . $this->calc2($input) . PHP_EOL;
     }
 
     private function calc(iterable $input)
@@ -73,5 +73,50 @@ class Solver2018_02 extends Command
         }
 
         return $countArray[2] * $countArray[3];
+    }
+
+    private function calc2(iterable $input) {
+        $inArray = [];
+        foreach ($input as $line) {
+            $inArray[] = $line;
+        }
+
+        $minDiffer = PHP_INT_MAX;
+        $lhsIndex = 0;
+        $rhsIndex = 0;
+        $ans = '';
+        for ($i = 0; $i < count($inArray) - 1; $i++) {
+            for ($j = $i + 1; $j < count($inArray); $j++) {
+                list($differ, $samepart) = $this->differ($inArray[$i], $inArray[$j], $minDiffer);
+                if ($differ < $minDiffer) {
+                    $minDiffer = $differ;
+                    $ans = $samepart;
+                    $lhsIndex = $i;
+                    $rhsIndex = $j;
+                }
+            }
+        }
+
+        $this->info("pair(L) : [" . str_pad($lhsIndex, 4) . "]{$inArray[$lhsIndex]}");
+        $this->info("pair(R) : [" . str_pad($rhsIndex, 4) . "]{$inArray[$rhsIndex]}");
+
+        return $ans;
+    }
+
+    private function differ(string $l, string $r, int $threshold) {
+        $differ = 0;
+        $samepart = [];
+        for ($i = 0; $i < strlen($l); $i++) {
+            if ($l[$i] !== $r[$i]) {
+                $differ++;
+                if ($differ >= $threshold) {
+                    return [$threshold, ""];
+                }
+            } else {
+                $samepart[] = $l[$i];
+            }
+        }
+
+        return [$differ, implode('', $samepart)];
     }
 }
