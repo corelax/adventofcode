7fabb843fc5a4ac148d29103178c8912459f990c
corelax <5426101+corelax@users.noreply.github.com>
2018-12-05 05:25:54 +0900

    feat: add solver 2018 day04 part1

---
+++ app/Console/Commands/Solvers/Y2018/Day04/Solver.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Console\Commands\Solvers\Y2018\Day04;
+
+use Iterator;
+use ArrayObject;
+use Carbon\Carbon;
+use Log;
+
+class Solver
+{
+    public function solvePart1(iterable $input)
+    {
+        $data = $this->regularData($input);
+
+        $sleepMemory = [];
+
+        $guard = null;
+        $sleepStart = null;
+        foreach ($data as $line) {
+            list($time, $action, $id) = $line;
+
+            if (is_null($guard) && $action != 'Guard') {
+                // I don't know who the the guard is
+                continue;
+            }
+
+            // adjust to [00:00..01:00)
+            if ($time->hour != 0) {
+                $minute = 0;
+            } else {
+                $minute = $time->minute;
+            }
+
+            Log::debug("$time : $action : $id  -> $minute");
+
+            switch ($action) {
+                case 'Guard':
+                    $guard = $id;
+                    if (! isset($sleepMemory[$guard])) {
+                        $sleepMemory[$guard] = array_fill(0, 60, 0);
+                    }
+                    break;
+                case 'falls':
+                    if (is_null($sleepStart)) {
+                        $sleepStart = $minute;
+                    } else {
+                        // already sleep
+                    }
+                    break;
+                case 'wakes':
+                    if (! is_null($sleepStart)) {
+                        // not include wakes up time
+                        for ($i = $sleepStart; $i < $minute; $i++) {
+                            $sleepMemory[$guard][$i]++;
+                        }
+
+                        $sleepStart = null;
+                    } else {
+                        // already wake up
+                    }
+                    break;
+            }
+        }
+
+        $mostSleptId = null;
+        $mostSleptMinutes = 0;
+        foreach ($sleepMemory as $guard => $memory) {
+            Log::info("ID $guard : " . array_sum($memory) . " minutes");
+            $sleptMinutes = array_sum($memory);
+            if ($sleptMinutes > $mostSleptMinutes) {
+                $mostSleptMinutes = $sleptMinutes;
+                $mostSleptId = $guard;
+            }
+        }
+
+        $theMinute = null;
+        $maxTimes = 0;
+        foreach ($sleepMemory[$mostSleptId] as $minute => $times) {
+            if ($times > $maxTimes) {
+                $maxTimes = $times;
+                $theMinute = $minute;
+            }
+        }
+
+        return $mostSleptId * $theMinute;
+    }
+
+    private function regularData(iterable $input)
+    {
+        if ($input instanceof Iterator) {
+            $sorted = [];
+            foreach ($input as $line) {
+                $sorted[] = $line;
+            }
+            sort($sorted);
+        } elseif (is_array($input)) {
+            $sorted = $input;
+            sort($sorted);
+        }
+
+        $ret = [];
+        foreach ($sorted as $line) {
+            $ret[] = $this->parseLine($line);
+        }
+
+        return $ret;
+    }
+
+    private function parseLine(string $line)
+    {
+        $arr = explode(' ', $line);
+        $time = Carbon::parse(substr($arr[0], 1) . " " . substr($arr[1], 0, -1));
+        $action = $arr[2];
+        if ($action == 'Guard') {
+            $id = intval(substr($arr[3], 1));
+        } else {
+            $id = null;
+        }
+
+        return [$time, $action, $id];
+    }
+}
---
+++ tests/Unit/Solver2018_04Test.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Console\Commands\Solvers\Y2018\Day04\Solver;
+use ArrayObject;
+use Carbon\Carbon;
+
+class Solver2018_04Test extends TestCase
+{
+    /**
+     * invoke method anyway.
+     */
+    private function invoke(Solver $obj, string $name, array $args)
+    {
+        $ref = new \ReflectionClass(Solver::class);
+        $method = $ref->getMethod($name);
+
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($obj, $args);
+    }
+
+    public function testSolverParseLine()
+    {
+        $solver = new Solver();
+
+        $data = $this->invoke($solver, 'parseLine', [
+            '[1518-11-01 00:00] Guard #10 begins shift',
+        ]);
+
+        $this->assertInstanceOf(Carbon::class, $data[0]);
+        $this->assertEquals(Carbon::parse('1518-11-01 00:00'), $data[0]);
+        $this->assertEquals('Guard', $data[1]);
+        $this->assertEquals(10, $data[2]);
+
+        $data = $this->invoke($solver, 'parseLine', [
+            '[1518-11-01 00:05] falls asleep',
+        ]);
+
+        $this->assertEquals('falls', $data[1]);
+
+        $data = $this->invoke($solver, 'parseLine', [
+            '[1518-11-01 00:25] wakes up',
+        ]);
+
+        $this->assertEquals('wakes', $data[1]);
+    }
+
+    public function testSolvePart1()
+    {
+        $solver = new Solver();
+
+        $this->assertEquals($solver->solvePart1($this->testData), 240);
+    }
+
+    private $testData = [
+        '[1518-11-01 00:00] Guard #10 begins shift',
+        '[1518-11-01 00:05] falls asleep',
+        '[1518-11-01 00:25] wakes up',
+        '[1518-11-01 00:30] falls asleep',
+        '[1518-11-01 00:55] wakes up',
+        '[1518-11-01 23:58] Guard #99 begins shift',
+        '[1518-11-02 00:40] falls asleep',
+        '[1518-11-02 00:50] wakes up',
+        '[1518-11-03 00:05] Guard #10 begins shift',
+        '[1518-11-03 00:24] falls asleep',
+        '[1518-11-03 00:29] wakes up',
+        '[1518-11-04 00:02] Guard #99 begins shift',
+        '[1518-11-04 00:36] falls asleep',
+        '[1518-11-04 00:46] wakes up',
+        '[1518-11-05 00:03] Guard #99 begins shift',
+        '[1518-11-05 00:45] falls asleep',
+        '[1518-11-05 00:55] wakes up',
+    ];
+}
