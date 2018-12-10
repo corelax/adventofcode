f63c5759b6a360a36e0af07a1a56fd8dc90e1d98
corelax <5426101+corelax@users.noreply.github.com>
2018-12-10 22:53:13 +0900

    refactor: make a model and process with the model

--- app/Console/Commands/Solvers/Y2018/Day09/SolverWithArray.php
+++ app/Console/Commands/Solvers/Y2018/Day09/SolverWithArray.php
@@ -18,45 +18,20 @@ class SolverWithArray
         return $result;
     }
 
-    // circle [
-    //     0    : marble0.next
-    //     1    : marble0.prev
-    //     2    : marble1.next
-    //     3    : marble1.prev
-    //     4    : marble2.next
-    //     ...
-    //     N*2  : marbleN.next
-    //     N*2+1: marbleN.next
-    // ]
-    // both next and prev has a marble value(not a index of circle)
-    // circle index can get with marble value ( *2 or *2+1 )
-    private $circle;
-
     private function playGame($playersCount, $lastMarble)
     {
         ini_set('memory_limit', '768M');
-        // echo "$playersCount, $lastMarble\n";
-        $this->circle = array_fill(0, ($lastMarble + 1) * 2, -1);
-
-        $this->circle[0] = 0;
-        $this->circle[1] = 0;
-
-        $current = 0;
+        $model = $this->initModel($lastMarble);
+        $model->showCircle();
 
         $scoreBoard = array_fill(1, $playersCount, 0);
 
         $player = 1;
-        $marble = 1;
+        for ($marble = 1; $marble <= $lastMarble; $marble++) {
+            $scoreBoard[$player] += $model->processMarble($marble);
 
-        $this->showCircle($current);
+            $model->showCircle();
 
-        while ($marble <= $lastMarble) {
-            list($current, $score) = $this->processMarble($current, $marble);
-            $scoreBoard[$player] += $score;
-
-            $this->showCircle($current);
-
-            $marble++;
             $player = ($player % $playersCount) + 1;
 
             // echo "$marble, $player\n";
@@ -67,59 +42,92 @@ class SolverWithArray
         return max($scoreBoard);
     }
 
-    private function showCircle($current)
+    private function initModel($lastMarble)
     {
-        return; // nothing
-        $p = 0;
-        do {
-            if ($p == $current) {
-                echo '(' . $p . ')' . ' ';
-            } else {
-                echo $p . ' ';
+        return new class($lastMarble) {
+            // circle [
+            //     0    : marble0.next
+            //     1    : marble0.prev
+            //     2    : marble1.next
+            //     3    : marble1.prev
+            //     4    : marble2.next
+            //     ...
+            //     N*2  : marbleN.next
+            //     N*2+1: marbleN.next
+            // ]
+            // both next and prev has a marble value(not a index of circle)
+            // circle index can get with marble value ( *2 or *2+1 )
+            private $circle;
+            private $current;
+
+            public function __construct(int $lastMarble)
+            {
+                $this->circle = array_fill(0, ($lastMarble + 1) * 2, -1);
+
+                $this->circle[0] = 0;
+                $this->circle[1] = 0;
+
+                $this->current = 0;
             }
-            $p = $this->circle[$p * 2];
-        } while (0 != $p);
-        echo PHP_EOL;
-    }
 
-    private function processMarble($current, $marble)
-    {
-        $score = 0;
-        if ($marble % 23 == 0) {
-            foreach (range(1, 7) as $i) {
-                $current = $this->circle[$current * 2 + 1];
+            public function processMarble($marble)
+            {
+                $current = $this->current;
+                $score = 0;
+                if ($marble % 23 == 0) {
+                    foreach (range(1, 7) as $i) {
+                        $current = $this->circle[$current * 2 + 1];
+                    }
+                    $score = $marble + $current;
+                    $next = $this->remove($current);
+                } else {
+                    foreach (range(1, 2) as $i) {
+                        $current = $this->circle[$current * 2];
+                    }
+                    $next = $this->insert($current, $marble);
+                }
+
+                $this->current = $next;
+                return $score;
             }
-            $score = $marble + $current;
-            $next = $this->remove($current);
-        } else {
-            foreach (range(1, 2) as $i) {
-                $current = $this->circle[$current * 2];
+
+            public function showCircle()
+            {
+                return; // nothing
+                $current = $this->current;
+                $p = 0;
+                do {
+                    if ($p == $current) {
+                        echo '(' . $p . ')' . ' ';
+                    } else {
+                        echo $p . ' ';
+                    }
+                    $p = $this->circle[$p * 2];
+                } while (0 != $p);
+                echo PHP_EOL;
             }
-            $next = $this->insert($current, $marble);
-        }
 
-        return [$next, $score];
-    }
+            // returns new current
+            private function insert($current, $value)
+            {
+                $this->circle[$value * 2] = $current;
+                $this->circle[$value * 2 + 1] = $this->circle[$current * 2 + 1];
 
-    // returns new current
-    private function insert($current, $value)
-    {
-        $this->circle[$value * 2] = $current;
-        $this->circle[$value * 2 + 1] = $this->circle[$current * 2 + 1];
+                $this->circle[($this->circle[$current * 2 + 1]) * 2] = $value;
+                $this->circle[$current * 2 + 1] = $value;
 
-        $this->circle[($this->circle[$current * 2 + 1]) * 2] = $value;
-        $this->circle[$current * 2 + 1] = $value;
+                return $value;
+            }
 
-        return $value;
-    }
+            // returns new current
+            private function remove($current)
+            {
+                $next = $this->circle[$current * 2];
+                $this->circle[($this->circle[$current * 2 + 1]) * 2]= $next;
+                $this->circle[($this->circle[$current * 2]) * 2 + 1]= $this->circle[$current * 2 + 1];
 
-    // returns new current
-    private function remove($current)
-    {
-        $next = $this->circle[$current * 2];
-        $this->circle[($this->circle[$current * 2 + 1]) * 2]= $next;
-        $this->circle[($this->circle[$current * 2]) * 2 + 1]= $this->circle[$current * 2 + 1];
-
-        return $next;
+                return $next;
+            }
+        };
     }
 }
