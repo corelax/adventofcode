21e0733cda67fb5da7956210c93e5afdc5dc223e
corelax <5426101+corelax@users.noreply.github.com>
2018-12-11 04:07:50 +0900

    feat: add solver 2018 day10 part1

---
+++ app/Console/Commands/Solvers/Y2018/Day10/Main.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands\Solvers\Y2018\Day10;
+
+use Illuminate\Console\Command;
+use Storage;
+use SplFileObject;
+
+class Main extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'solver:2018_10';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Day 10: The Stars Align';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+        $input = new SplFileObject(storage_path('app/input/2018/day10/input.dat'));
+        $input->setFlags(SplFileObject::DROP_NEW_LINE | SplFileObject::READ_AHEAD | SplFileObject::SKIP_EMPTY);
+
+        $solver = new Solver();
+
+        echo "part1 : " . PHP_EOL;
+        foreach ($solver->solvePart1($input) as $line) {
+            echo $line . PHP_EOL;
+        }
+    }
+}
---
+++ app/Console/Commands/Solvers/Y2018/Day10/Solver.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Console\Commands\Solvers\Y2018\Day10;
+
+class Solver
+{
+    public function solvePart1(iterable $input)
+    {
+        $infos = [];
+        foreach ($input as $line) {
+            $a = explode(',', str_replace(['<', '>'], ',', $line));
+            $infos[] = [
+                intval($a[1]), // posX
+                intval($a[2]), // posY
+                intval($a[4]), // vX
+                intval($a[5]), // vY
+            ];
+        }
+
+
+        // get how long time elapsed
+
+        $foundY = [];
+        $ylist_vero = [];
+        foreach ($infos as $info) {
+            $ylist_vero[$info[3]][] = $info[1];
+        }
+        // ksort($ylist_vero);
+        $heights = [];
+        foreach ($ylist_vero as $v => $ylist) {
+            $heights[$v] = max($ylist) - min($ylist);
+        }
+
+        $foundY = array_keys($heights, max($heights));
+
+        if (count($foundY) < 2) {
+            echo 'sorry' . PHP_EOL;
+            exit(1);
+        }
+
+        // if foundY are 1, 2
+        // min($ylist[1]) + $n == min($ylist[2]) + 2 * $n;
+        $timeElapsed = (min($ylist_vero[$foundY[0]]) - min($ylist_vero[$foundY[1]])) / ($foundY[1] - $foundY[0]);
+        // ksort($ylist_vero);
+        // foreach ($ylist_vero as $v => $ylist) {
+        //     echo $v . ' ' . (min($ylist) + $v * $timeElapsed) . PHP_EOL;
+        // }
+
+        $map = [];
+        $minX = $minY = PHP_INT_MAX;
+        $maxX = $maxY = 0;
+        foreach ($infos as $info) {
+            $x = $info[0] + $info[2] * $timeElapsed;
+            $y = $info[1] + $info[3] * $timeElapsed;
+            $minX = min([$x, $minX]);
+            $minY = min([$y, $minY]);
+            $maxX = max([$x, $maxX]);
+            $maxY = max([$y, $maxY]);
+            $map["$x,$y"] = true;
+        }
+
+        $result = [];
+        foreach (range($minY, $maxY) as $y) {
+            $line = [];
+            foreach (range($minX, $maxX) as $x) {
+                if (isset($map["$x,$y"])) {
+                    $line[] = "#";
+                } else {
+                    $line[] = ".";
+                }
+            }
+            $result[$y - $minY] = implode('', $line);
+        }
+
+        return $result;
+    }
+}
