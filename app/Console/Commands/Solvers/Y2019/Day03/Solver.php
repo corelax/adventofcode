77b24b5997aa6af12da202dca442d63c60c4bea4
corelax <5426101+corelax@users.noreply.github.com>
2019-12-08 17:46:02 +0900

    feat: add solver 2019 day03 part1

---
+++ app/Console/Commands/Solvers/Y2019/Day03/Solver.php
@@ -0,0 +1,72 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Console\Commands\Solvers\Y2019\Day03;
+
+class Solver
+{
+    public function solvePart1(iterable $input): int
+    {
+        $routes = $this->parseInput($input);
+
+        $map = [];
+        $minDistance = PHP_INT_MAX;
+
+        // start
+        $s_x = 0;
+        $s_y = 0;
+
+        $routeIndex = 0;
+        foreach ($routes as $route) {
+            // current
+            $x = $s_x;
+            $y = $s_y;
+
+            $routeIndex++;
+            foreach ($route as $command) {
+                switch($command['d']) {
+                    case 'R': $dx =  1; $dy =  0; break;
+                    case 'L': $dx = -1; $dy =  0; break;
+                    case 'U': $dx =  0; $dy =  1; break;
+                    case 'D': $dx =  0; $dy = -1; break;
+                }
+                for ($i = 0; $i < $command['l']; $i++) {
+                    $x += $dx;
+                    $y += $dy;
+                    if (isset($map[$x][$y]) && $map[$x][$y] != $routeIndex) {
+                        $distance = self::distance($s_x, $s_y, $x, $y);
+                        $minDistance = min($distance, $minDistance);
+                    }
+                    $map[$x][$y] = $routeIndex;
+                }
+            }
+        }
+
+        return $minDistance;
+    }
+
+    public function solvePart2(iterable $input): int
+    {
+        $this->parseInput($input);
+        return -1;
+    }
+
+    private function parseInput(iterable $input): array
+    {
+        $routes = [];
+        foreach ($input as $line) {
+            $route = [];
+            foreach (explode(',', $line) as $command) {
+                $route[] = ['d' => $command[0], 'l' => substr($command, 1)];
+            }
+            $routes[] = $route;
+        }
+
+        return $routes;
+    }
+
+    private static function distance(int $x1, int $y1, int $x2, int $y2): int
+    {
+        return abs($x2 - $x1) + abs($y2 - $y1);
+    }
+}
---
+++ tests/Unit/2019/Day03Test.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Console\Commands\Solvers\Y2019\Day03\Solver;
+
+class Solver2019_03Test extends TestCase
+{
+    private Solver $solver;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->solver = new Solver();
+    }
+
+    public function testSolvePart1()
+    {
+        $this->assertEquals(6, $this->solver->solvePart1([
+            'R8,U5,L5,D3',
+            'U7,R6,D4,L4',
+        ]));
+        $this->assertEquals(159, $this->solver->solvePart1([
+            'R75,D30,R83,U83,L12,D49,R71,U7,L72',
+            'U62,R66,U55,R34,D71,R55,D58,R83',
+        ]));
+        $this->assertEquals(135, $this->solver->solvePart1([
+            'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51',
+            'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7',
+        ]));
+    }
+
+    // public function testSolvePart2()
+    // {
+    // }
+
+    /**
+     * invoke method anyway.
+     */
+    private function invoke(Solver $obj, string $name, array $args)
+    {
+        $ref = new \ReflectionClass(Solver::class);
+        $method = $ref->getMethod($name);
+
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($obj, $args);
+    }
+}
